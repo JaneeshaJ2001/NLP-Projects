99b526104065753a2a21dd974c1cc04facd138b6
JaneeshaJ2001 <wickramasinghejj.21@uom.lk>
2024-09-14 23:02:16 +0530

    initialize

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+## load the GROQ And OpenAI API KEY 
+groq_api_key=os.getenv('GROQ_API_KEY')
+os.environ["GOOGLE_API_KEY"]=os.getenv("GOOGLE_API_KEY")
+
+st.title("Gemma Model Document Q&A")
+
+llm=ChatGroq(groq_api_key=groq_api_key,
+             model_name="Llama3-8b-8192")
+print(llm)
